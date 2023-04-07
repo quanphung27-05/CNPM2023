786192585766907f75d0198b814583838f502be3
quanphung27-05 <100451205+quanphung27-05@users.noreply.github.com>
2023-04-07 16:45:07 +0700

    fix bug

--- js/slider.js
+++ js/slider.js
@@ -13,3 +13,17 @@ stars.forEach((star, index1) => {
   });
 });
 
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  grabCursor: true,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
