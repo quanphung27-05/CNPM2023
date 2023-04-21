56f84457274c29471d8ffe0cb2779014a5d266bc
quanphung27-05 <100451205+quanphung27-05@users.noreply.github.com>
2023-04-21 22:57:47 +0700

    update detail-page

--- js/login.js
+++ js/login.js
@@ -23,3 +23,16 @@ function closeModal() {
 modalButton.addEventListener("click", openModal);
 closeButton.addEventListener("click", closeModal);
 
+var swiper = new Swiper('.blog-slider', {
+  spaceBetween: 30,
+  effect: 'fade',
+  loop: true,
+  mousewheel: {
+    invert: false,
+  },
+  // autoHeight: true,
+  pagination: {
+    el: '.blog-slider__pagination',
+    clickable: true,
+  }
+});
